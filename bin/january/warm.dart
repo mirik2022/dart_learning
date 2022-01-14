7272fa2f3e27bc2a1af665a47a4079015bde924d
mirik2022 <miro20061122@gmail.com>
2022-01-14 18:27:11 +0300

    задача о червяке попытка 1

---
+++ bin/january/warm.dart
@@ -0,0 +1,15 @@
+void main(List<String> arguments) {
+  num length = 22;
+  num step_forward = 3;
+  num step_backward = 1;
+
+  num total_steps =
+      calculate_total_steps_forward(length, step_forward, step_backward);
+  print(total_steps);
+}
+
+num calculate_total_steps_forward(
+    num length, num step_forward, num step_backward) {
+  // calculate
+  return 10;
+}

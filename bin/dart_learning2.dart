4477b9b06271980b8a1167898c525ac953c38c0f
mirik2022 <miro20061122@gmail.com>
2022-01-14 18:10:55 +0300

    project create

---
+++ bin/dart_learning2.dart
@@ -0,0 +1,5 @@
+import 'package:dart_learning2/dart_learning2.dart' as dart_learning2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_learning2.calculate()}!');
+}
---
+++ test/dart_learning2_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_learning2/dart_learning2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

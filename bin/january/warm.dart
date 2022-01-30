67eaaef12c1b8526aacc3c5c8f2a531a1dda5b57
mirik2022 <miro20061122@gmail.com>
2022-01-30 23:13:14 +0300

    задача про три числа

--- bin/january/warm.dart
+++ bin/january/warm.dart
@@ -1,23 +1,17 @@
-void main(List<String> arguments) {
-  int length = 10;
-  int step_forward = 3;
-  int fall_backward = 1;
-
-  var total_steps =
-      calculate_total_steps_forward(length, step_forward, fall_backward);
-  print(total_steps);
+/*Даны 3 числа. Найти наибольшую сумму двух чисел из них*/
+void main() {
+  var i = u(9, 7, 37);
 }
 
-calculate_total_steps_forward(int length, int step_forward, int fall_backward) {
-  // calculate
-  var l = 0;
-  var p = 0;
-  while (p < length) {
-    p = p + step_forward;
-    l = l + 1;
-    if (p < length) {
-      p = p - fall_backward;
-    }
-    print(l);
+u(int t, int y, int o) {
+  var h = t + y;
+  var g = y + o;
+  var c = t + o;
+  if (h > g && h > c) {
+    print(h);
+  } else if (g > h && g > c) {
+    print(g);
+  } else if (c > g && c > h) {
+    print(c);
   }
 }
